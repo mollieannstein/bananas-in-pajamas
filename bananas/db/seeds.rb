fb827ea6e816f58640178d042f7322af0619cb45
Mollie Stein <mastein109@gmail.com>
2016-04-18 14:18:11 -0500

    Add models, migrations, associations

--- bananas/db/seeds.rb
+++ bananas/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create!(username: "yaboichrissyb", email: "yaboi@gmail.com", password: 'password', admin_status: true)
+user2 = User.create!(username: "boolean", email: "boo@gmail.com", password: 'password', admin_status: false)
+
+article = Article.create!(creator_id: 1, featured: true, title: "Bananas", content: Faker::Hipster.paragraph)
+
+category = Category.create!(name: "Tropical Fruits")
+
+tagging = Tagging.create!(article_id: 1, category_id: 1)
+
+revision = Revision.create!(article_id: 1, revisor_id: 2, revised_content: "I revised this BOO")
