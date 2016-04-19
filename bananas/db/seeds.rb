70afa2ab676f75dec442f04e63fe2697d9aa9819
Joseph Scott & Chris Bunkers <pair+scottjoseph.yaboichrissyb@devbootcamp.com>
2016-04-19 10:15:42 -0500

    add seeds and header/sidebar layout

--- bananas/db/seeds.rb
+++ bananas/db/seeds.rb
@@ -1,10 +1,40 @@
+User.delete_all
+Article.delete_all
+Tagging.delete_all
+Revision.delete_all
+Category.delete_all
+
 user = User.create!(username: "yaboichrissyb", email: "yaboi@gmail.com", password: 'password', admin_status: true)
-user2 = User.create!(username: "boolean", email: "boo@gmail.com", password: 'password', admin_status: false)
+user2 = User.create!(username: "boolean", email: "boo@gmail.com", password: 'password', admin_status: true)
+
+general_users = 10.times.map do
+  User.create!( username: Faker::Internet.user_name,
+                email: Faker::Internet.email,
+                password: 'password',
+                admin_status: false
+                )
+end
+
+categories =  5.times.map do
+  Category.create!(name: Faker::Team.sport)
+end
+
+non_featured_articles = 20.times do
+  Article.create!(creator_id: general_users.sample.id, featured: false , title: Faker::Hipster.word, content: Faker::Hipster.paragraph(25))
+end
+
+featured_articles = 5.times do
+  Article.create!(creator_id: general_users.sample.id, featured: true , title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph(25))
+end
+
+taggings = 5.times.map do
+  Tagging.create!(article_id: Article.all.sample.id, category_id: categories.sample.id)
+end
 
 article = Article.create!(creator_id: 1, featured: true, title: "Bananas", content: Faker::Hipster.paragraph)
 
-category = Category.create!(name: "Tropical Fruits")
 
 tagging = Tagging.create!(article_id: 1, category_id: 1)
 
 revision = Revision.create!(article_id: 1, revisor_id: 2, revised_content: "I revised this BOO")
+
